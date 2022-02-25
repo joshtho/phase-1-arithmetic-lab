8f47ca5caae0bf8f095e718c5872ebfeacc26732
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-02-24 20:22:45 -0600

    commit tree e604eefa3f8e31067cfe96413c4ca592023b56e2

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 10;
+let num2 = 6;
+const multiply = (num1 * num2) + 2
+const random = Math.ceil(Math.random() * 10);
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4
+const max = Math.max(5,10,15,20);
 // Write your code here
